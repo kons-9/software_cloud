56cd0ee652fc242371d51ff2c01b534ffd81b62c
toshiki goto <gotoshi.euphonium@gmail.com>
2023-05-19 15:25:34 +0900

    Update

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
-from flask import Flask, request
+from flask import Flask, request, render_template, redirect
 
-app = Flask(__name__, static_folder='.', static_url_path='')
-data = []
+app = Flask(__name__, static_folder='.', static_url_path='', template_folder='view')
+items = []
 
 @app.route('/')
 def index():
-    return app.send_static_file('index.html')
+    return render_template('index.html')
 
 @app.route('/append', methods=['GET', 'POST'])
 def append():
     if request.method == 'POST':
-        data.append(request.form['message'])
-    return data
+        append_data =request.form['todo_item'] 
+        items.append(append_data)
+        return render_template('append.html', item=append_data)
+    else:
+        return redirect('/')
+
+@app.route('/see_all_item')
+def see_all_item():
+    return render_template('see_all_item.html', items=items)
+
+
+
 
 
 app.run(port=8080, debug=True)
